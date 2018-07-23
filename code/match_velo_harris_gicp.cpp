853ecbace356d3373cecea03b05f08ace343d019
Libby Boroson <boroson@usc.edu>
2018-07-23 12:07:49 -0700

    Basic GICP working, adding my own covariances

--- code/match_velo_harris_gicp.cpp
+++ code/match_velo_harris_gicp.cpp
@@ -10,9 +10,6 @@
 #include <vector>
 #include <string>
 
-//#include <opencv2/core.hpp>
-//#include <opencv2/highgui.hpp>
-
 #include <boost/thread/thread.hpp>
 #include <pcl/range_image/range_image.h>
 #include <pcl/io/pcd_io.h>
@@ -26,7 +23,6 @@
 #include <pcl/keypoints/harris_3d.h>
 
 #include "load_velodyne.h"
-#include "load_stereo_cam.h"
 
 using namespace std;
 
@@ -38,11 +34,11 @@ int main(int argc, char** argv) {
 
     cout << "loading stereo frame 1 from file" << endl;
 
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cam_frame0_in_cam0_frame_ptr = loadCamFrameInCamiRef(20);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr velo_frame1_in_cam0_frame_ptr = loadVelFrameInCamiRef(1);
 
     std::cout << "Saved " << velo_frame0_in_cam0_frame_ptr->points.size () << " data points from frame 0"
               << std::endl;
-    std::cout << "Saved " << cam_frame0_in_cam0_frame_ptr->points.size () << " data points from frame 1"
+    std::cout << "Saved " << velo_frame1_in_cam0_frame_ptr->points.size () << " data points from frame 1"
               << std::endl;
 
     pcl::HarrisKeypoint3D <pcl::PointXYZ, pcl::PointXYZI> detector;
@@ -54,6 +50,11 @@ int main(int argc, char** argv) {
 //    pcl::StopWatch watch;
     detector.compute (*keypoints_frame0_ptr);
     pcl::console::print_highlight ("Detected %zd points in frame 0\n", keypoints_frame0_ptr->size ());
+    pcl::GeneralizedIterativeClosestPoint<pcl::PointXYZI,pcl::PointXYZI>::MatricesVector keypoints_frame0_cov;
+    for (int i = 0; i < keypoints_frame0_ptr->size(); ++i)
+    {
+        keypoints_frame0_cov.push_back(Eigen::Matrix3f::Identity());
+    }
 //    pcl::PointIndicesConstPtr keypoints_indices_frame0 = detector.getKeypointsIndices ();
 //    if (!keypoints_indices_frame0->indices.empty ())
 //    {
@@ -64,7 +65,7 @@ int main(int argc, char** argv) {
 //        pcl::console::print_warn ("Keypoints indices are empty!\n");
 
     pcl::PointCloud<pcl::PointXYZI>::Ptr keypoints_frame1_ptr (new pcl::PointCloud<pcl::PointXYZI>);
-    detector.setInputCloud (cam_frame0_in_cam0_frame_ptr);
+    detector.setInputCloud (velo_frame1_in_cam0_frame_ptr);
 //    detector.setRadius(2.0);
     detector.compute (*keypoints_frame1_ptr);
     pcl::console::print_highlight ("Detected %zd points in frame 1\n", keypoints_frame1_ptr->size ());
@@ -99,9 +100,9 @@ int main(int argc, char** argv) {
     // --------------------------------------------
 //    pcl::visualization::PCLVisualizer viewer("3D Viewer");
 //    viewer.setBackgroundColor(1, 1, 1);
-    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> frame1_cam0_color_handler(cam_frame0_in_cam0_frame_ptr, 255,
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> frame1_cam0_color_handler(velo_frame1_in_cam0_frame_ptr, 255,
                                                                                                                 0, 0);
-    viewer.addPointCloud(cam_frame0_in_cam0_frame_ptr, frame1_cam0_color_handler, "frame 1");
+    viewer.addPointCloud(velo_frame1_in_cam0_frame_ptr, frame1_cam0_color_handler, "frame 1");
     viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "frame 1");
 ////    viewer.addCoordinateSystem (1.0f, "global");
 ////    pcl::visualization::PointCloudColorHandlerCustom<PointType> point_cloud_color_handler (&cloud_velo, 150, 150, 150);
@@ -122,10 +123,11 @@ int main(int argc, char** argv) {
     for (int i = 0; i < 10; ++i) {
         pcl::GeneralizedIterativeClosestPoint<pcl::PointXYZI, pcl::PointXYZI> icp;
         icp.setInputSource(cloud_source);
+        icp.setSourceCovariances(&keypoints_frame0_cov);
         icp.setInputTarget(keypoints_frame1_ptr);
         icp.setTransformationEpsilon(1e-13);
         icp.setEuclideanFitnessEpsilon(0.1);
-        icp.setMaxCorrespondenceDistance (20.0);
+        icp.setMaxCorrespondenceDistance (1.0);
 //        icp.setRANSACOutlierRejectionThreshold (1.0);
         pcl::PointCloud<pcl::PointXYZI> Final;
         icp.align(Final);
